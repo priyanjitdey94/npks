bcdf550398c0a53e3e4ba4eb053cb6b76dd99c2b
priyanjitdey94 <priyanjit.dey@gmail.com>
2024-03-16 17:55:50 +0530

    fixed

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,4 +1,3 @@
-import styles from '@/styles/Table.module.css';
 
 const Table = () => {
     return (
--- src/pages/table.tsx
+++ src/pages/table.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
 import { useQuery } from "@tanstack/react-query";
 import Table from "@/components/Table";
 
